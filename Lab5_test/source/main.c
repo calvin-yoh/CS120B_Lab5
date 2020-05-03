b9ec041c64a7920ea4206abd219e5e56ce784665
Calvin Yoh <flashfire340@gmail.com>
2020-05-02 19:01:46 -0700

    Part 1

--- Lab5_test/source/main.c
+++ Lab5_test/source/main.c
@@ -13,11 +13,48 @@
 #endif
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRB = 0xFF;    PORTB = 0x00;
-    /* Insert your solution below */
-    while (1) {
-       PORTB = 0x0F;
-    }
-    return 1;
+	int main(void) {
+		DDRA = 0x00; PORTA = 0xFF;
+		DDRC = 0xFF; PORTC = 0x00;
+		unsigned char tempA = 0x00;
+		unsigned char tempC = 0x00;
+
+		while (1) {
+
+			tempA = ~PINA;
+
+			if (tempA == 0x00)
+			{
+				tempC = 0x40;
+			}
+			else if (tempA == 0x01 || tempA == 0x02)
+			{
+				tempC = 0x60;
+			}
+			else if (tempA == 0x03 || tempA == 0x04)
+			{
+				tempC = 0x70;
+			}
+			else if (tempA == 0x05 || tempA == 0x06)
+			{
+				tempC = 0x38;
+			}
+			else if (tempA == 0x07 || tempA == 0x08 || tempA == 0x09)
+			{
+				tempC = 0x3C;
+			}
+			else if (tempA == 0x0A || tempA == 0x0B || tempA == 0x0C)
+			{
+				tempC = 0x3E;
+			}
+			else
+			{
+				tempC = 0x3F;
+			}
+
+			PORTC = tempC;
+			tempC = 0x00;
+		}
+		return 0;
+	}
 }
